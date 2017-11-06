6066afdce534eb11f42b58b9994cdac6484f4659
Muneeb Samuels <muneeb.samuels@codebus.io>
2017-11-06 21:11:02 +0200

    export actions

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017-present Muneeb Samuels. All Rights Reserved. See License.txt for license information.
+ */
+
+import * as UserActions from './user'
+import * as AppActions from './app'
+import * as SearchActions from './search'
+import * as CustomerActions from './customer'
+
+export {
+  AppActions,
+  UserActions,
+  SearchActions,
+  CustomerActions
+}
