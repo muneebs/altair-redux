06b2ac0c0621787199f7e915996b6f1ec54d9689
Muneeb Samuels <muneeb.samuels@codebus.io>
2017-11-06 21:12:46 +0200

    service reducer exports

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2017-present Muneeb Samuels. All Rights Reserved. See License.txt for license information.
+ */
+
+import entities from './entities'
+import requests from './requests'
+
+export default {
+  entities,
+  requests
+}
