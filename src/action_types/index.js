2e0eb2f3a4d7828bd70dd58afc49983b9ec25f65
Muneeb Samuels <muneeb.samuels@codebus.io>
2017-11-06 21:10:48 +0200

    export action_types

---
+++ src/action_types/index.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017-present Muneeb Samuels. All Rights Reserved. See License.txt for license information.
+ */
+
+import UserTypes from './user'
+import CustomerTypes from './customer'
+import SearchTypes from './search'
+import AppTypes from './app'
+
+export {
+  UserTypes,
+  CustomerTypes,
+  SearchTypes,
+  AppTypes
+}
