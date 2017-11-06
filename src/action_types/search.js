8bd393f0a86ef6eb4aa48bc058af6cbb15e60c43
Muneeb Samuels <muneeb.samuels@codebus.io>
2017-11-06 21:10:30 +0200

    missing search action_types

---
+++ src/action_types/search.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017-present Muneeb Samuels. All Rights Reserved. See License.txt for license information.
+ */
+
+import keyMirror from 'utils/key_mirror'
+
+export default keyMirror({
+  SEARCH_CUSTOMERS_REQUEST: null,
+  SEARCH_CUSTOMERS_SUCCESS: null,
+  SEARCH_CUSTOMERS_FAILURE: null,
+
+  CLEAR_CUSTOMER_SEARCH_RESULTS: null,
+  RECEIVED_SEARCH_CUSTOMERS: null
+}, '@@redblade-redux/')
