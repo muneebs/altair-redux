5db6aabdd2c1cc9c5b8d5539f31f2154e76dad4a
Muneeb Samuels <muneeb.samuels@codebus.io>
2017-11-06 21:11:43 +0200

    request helpers and constants

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2017-present Muneeb Samuels. All Rights Reserved. See License.txt for license information.
+ */
+
+import RequestStatus from './request_status'
+
+export {
+  RequestStatus
+}
---
+++ src/constants/request_status.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2017-present Muneeb Samuels. All Rights Reserved. See License.txt for license information.
+ */
+
+import keyMirror from 'utils/key_mirror'
+
+export default keyMirror({
+  NOT_STARTED: null,
+  STARTED: null,
+  SUCCESS: null,
+  FAILURE: null,
+  CANCELLED: null
+})
